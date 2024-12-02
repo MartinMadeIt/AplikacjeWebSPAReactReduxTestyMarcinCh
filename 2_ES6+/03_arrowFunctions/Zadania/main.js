3754c827447e9c9b95d2a1868622e7f9f66cb2ea
MartinMadeIt <marcinvet94@gmail.com>
2024-12-02 12:49:48 +0100

    zadanie ES6 3

--- 2_ES6+/03_arrowFunctions/Zadania/main.js
+++ 2_ES6+/03_arrowFunctions/Zadania/main.js
@@ -1,1 +1,23 @@
+const getSecondMaxNumber = array => {
+  const maxNr = Math.max(...array);
+  const arrWithoutMax = array.filter(nr => nr !== maxNr)
+  return Math.max(...arrWithoutMax)
+}
 
+const runInterval = (n= 8) => {
+  if (n > 0 && n <= 10) {
+    let myInterval;
+    let index= 0;
+
+    myInterval = setInterval(() => {
+      console.log("Hello")
+      index++
+      if (index >= n) { clearInterval(myInterval) }
+    }, 500)
+
+  } else {
+    console.log('n ma mieć wartość 1- 10');
+  }
+}
+
+const generateLinks = array => array.map(link => `https://${link}.com`)
